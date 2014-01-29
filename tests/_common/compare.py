9da4ca7bf6f350a174605dfbf48947d2afc9fbce
Jonas Schulte-Coerne <jonas@schulte-coerne.de>
2014-01-29 21:55:53 +0100

    Add a parameter to skip the comparison of the labels for the two functions compare_signals_almost_equal and compare_spectrums_almost_equal in the _common module for the unit tests.

--- tests/_common/compare.py
+++ tests/_common/compare.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-def compare_signals_almost_equal(testcase, signal1, signal2, places=7):
+def compare_signals_almost_equal(testcase, signal1, signal2, places=7, compare_labels=True):
 	"""
 	Compares two Signals with a TestCase's almostEqual method.
 	The sampling rate and the labels are compared to be absolutely equal, while
@@ -22,11 +22,12 @@ def compare_signals_almost_equal(testcase, signal1, signal2, places=7):
 	@param testcase: the TestCase whose comparison methods shall be used.
 	@param signal1: the first Signal that shall be compared
 	@param signal2: the second Signal that shall be compared
+	@param compare_labels: True if the labels shall also be compared. False, if differing labels shall not lead to a failure
 	"""
 	testcase.assertEqual(signal1.GetSamplingRate(), signal2.GetSamplingRate())
-	compare_channeldatas_almost_equal(testcase=testcase, data1=signal1, data2=signal2, places=places)
+	compare_channeldatas_almost_equal(testcase=testcase, data1=signal1, data2=signal2, places=places, compare_labels=compare_labels)
 
-def compare_spectrums_almost_equal(testcase, spectrum1, spectrum2, places=7):
+def compare_spectrums_almost_equal(testcase, spectrum1, spectrum2, places=7, compare_labels=True):
 	"""
 	Compares two Spectrums with a TestCase's almostEqual method.
 	The resolution and the labels are compared to be absolutely equal, while
@@ -34,11 +35,12 @@ def compare_spectrums_almost_equal(testcase, spectrum1, spectrum2, places=7):
 	@param testcase: the TestCase whose comparison methods shall be used.
 	@param spectrum1: the first Spectrum that shall be compared
 	@param spectrum2: the second Spectrum that shall be compared
+	@param compare_labels: True if the labels shall also be compared. False, if differing labels shall not lead to a failure
 	"""
 	testcase.assertEqual(spectrum1.GetResolution(), spectrum2.GetResolution())
-	compare_channeldatas_almost_equal(testcase=testcase, data1=spectrum1, data2=spectrum2, places=places)
+	compare_channeldatas_almost_equal(testcase=testcase, data1=spectrum1, data2=spectrum2, places=places, compare_labels=compare_labels)
 
-def compare_channeldatas_almost_equal(testcase, data1, data2, places):
+def compare_channeldatas_almost_equal(testcase, data1, data2, places, compare_labels):
 	"""
 	Compares two ChannelData instances with a TestCase's almostEqual method.
 	The labels are compared to be absolutely equal, while the channels' samples
@@ -46,10 +48,12 @@ def compare_channeldatas_almost_equal(testcase, data1, data2, places):
 	@param testcase: the TestCase whose comparison methods shall be used.
 	@param data1: the first data set that shall be compared
 	@param data2: the second data set that shall be compared
+	@param compare_labels: True if the labels shall also be compared. False, if differing labels shall not lead to a failure
 	"""
 	testcase.assertEqual(len(data1.GetChannels()), len(data2.GetChannels()))
 	testcase.assertEqual(len(data1), len(data2))
-	testcase.assertEqual(data1.GetLabels(), data2.GetLabels())
+	if compare_labels:
+		testcase.assertEqual(data1.GetLabels(), data2.GetLabels())
 	for c in range(len(data1.GetChannels())):
 		for s in range(len(data1)):
 			testcase.assertAlmostEqual(data1.GetChannels()[c][s], data2.GetChannels()[c][s], places)
