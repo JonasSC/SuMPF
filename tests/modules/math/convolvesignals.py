dafefac729c136fc8569936222b00b52b7f879af
Jonas Schulte-Coerne <jonas@schulte-coerne.de>
2014-07-22 22:56:18 +0200

    The ConvolveSignals class now has the convolution mode SPECTRUM, which calculates the convolution in the frequency domain.

--- tests/modules/math/convolvesignals.py
+++ tests/modules/math/convolvesignals.py
@@ -38,23 +38,28 @@ class TestConvolveSignals(unittest.TestCase):
 		self.assertEqual(cnv.GetOutput().GetLabels(), ("Convolution 1",))			# check if the label has been set as expected
 		self.assertEqual(cnv.GetOutput().GetChannels(), ((0.0, 0.5, 0.0, 0.0),))	# check convolution result for mode ConvolveSignals.FULL
 		cnv.SetConvolutionMode(sumpf.modules.ConvolveSignals.SAME)
-		self.assertEqual(cnv.GetOutput().GetChannels(), ((0.0, 0.5, 0.0),))		# check convolution result for mode ConvolveSignals.SAME
+		self.assertEqual(cnv.GetOutput().GetChannels(), ((0.0, 0.5, 0.0),))			# check convolution result for mode ConvolveSignals.SAME
 		cnv.SetConvolutionMode(sumpf.modules.ConvolveSignals.VALID)
-		self.assertEqual(cnv.GetOutput().GetChannels(), ((0.5, 0.0),))			# check convolution result for mode ConvolveSignals.VALID
-		cnv.SetInput1(self.signal2)
+		self.assertEqual(cnv.GetOutput().GetChannels(), ((0.5, 0.0),))				# check convolution result for mode ConvolveSignals.VALID
 		cnv.SetInput2(self.signal1)
-		self.assertEqual(cnv.GetOutput().GetChannels(), ((0.5, 0.0),))			# check if setting the Signals works
+		cnv.SetConvolutionMode(sumpf.modules.ConvolveSignals.SPECTRUM)
+		spectrum1 = sumpf.modules.FourierTransform(signal=self.signal1).GetSpectrum()
+		reference = sumpf.modules.InverseFourierTransform(spectrum=spectrum1 * spectrum1).GetSignal()
+		self.assertEqual(cnv.GetOutput().GetChannels(), reference.GetChannels())	# check convolution result for mode ConvolveSignals.SPECTRUM
 
 	def test_errors(self):
 		"""
 		Tests if the convolution module raises errors correctly.
 		"""
 		cnv = sumpf.modules.ConvolveSignals(signal1=self.signal2, mode=sumpf.modules.ConvolveSignals.SAME)
-		self.assertRaises(ValueError, cnv.SetConvolutionMode, "Very Full")												# shall fail if mode is not supported by numpy
+		self.assertRaises(ValueError, cnv.SetConvolutionMode, "Very Full")									# shall fail if mode is not supported by numpy
 		cnv.SetInput2(sumpf.Signal(channels=((0.0, 0.0),), samplingrate=44100))
-		self.assertRaises(ValueError, cnv.GetOutput)																# shall fail because input Signals do not have the same sampling rate
-		cnv.SetInput2(sumpf.Signal(channels=((1.0, 0.0), (5.0, 5.0)), samplingrate=48000))						# adding a Signal with different channel count or different length shall not fail
-		self.assertEqual(cnv.GetOutput().GetChannels(), ((0.0, 0.5, 0.0),))
+		self.assertRaises(ValueError, cnv.GetOutput)														# shall fail because input Signals do not have the same sampling rate
+		cnv.SetInput2(sumpf.Signal(channels=((1.0, 0.0), (5.0, 5.0)), samplingrate=48000))
+		self.assertRaises(ValueError, cnv.GetOutput)														# shall fail because input Signals do not have the same number of channels
+		cnv.SetInput2(signal=self.signal1)
+		cnv.SetConvolutionMode(sumpf.modules.ConvolveSignals.SPECTRUM)
+		self.assertRaises(ValueError, cnv.GetOutput)														# shall fail because in SPECTRUM mode, the input Signals must have the same length, which these signals haven't
 
 	def test_connectors(self):
 		"""
