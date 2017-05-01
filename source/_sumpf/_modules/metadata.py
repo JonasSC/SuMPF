6d48243c63a73b99c21ab62e350be2badee40569
Jonas Schulte-Coerne <jonas@schulte-coerne.de>
2017-05-01 17:51:16 +0200

    AdjustSamplingRate, a class for redefining the sampling rate of a Signal

--- source/_sumpf/_modules/metadata.py
+++ source/_sumpf/_modules/metadata.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+from ._metadata.adjustsamplingrate import AdjustSamplingRate
 from ._metadata.channeldataproperties import ChannelDataProperties
 from ._metadata.copylabels import CopyLabelsToSignal, CopyLabelsToSpectrum
 from ._metadata.durationtolength import DurationToLength
--- tests/modules/metadata/__init__.py
+++ tests/modules/metadata/__init__.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+from .adjustsamplingrate import TestAdjustSamplingRate
 from .channeldataproperties import TestChannelDataProperties
 from .copylabels import TestCopyLabels
 from .durationtolength import TestDurationToLength
