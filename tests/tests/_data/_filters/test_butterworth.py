fcf65fb2b43b218011df1b1e20bffc8a5773590d
Jonas Schulte-Coerne <jonas@schulte-coerne.de>
2020-05-22 22:34:15 +0200

    Extended the tests for the ButterworthFilter class

--- tests/tests/_data/_filters/test_butterworth.py
+++ tests/tests/_data/_filters/test_butterworth.py
@@ -49,6 +49,19 @@ def test_lowpass_properties(cutoff_frequency, order):
 
 
 @hypothesis.given(cutoff_frequency=hypothesis.strategies.floats(min_value=1e-15, max_value=1e12),
+                  order=hypothesis.strategies.integers(min_value=1, max_value=16))
+def test_lowpass_with_scipy(cutoff_frequency, order):
+    """Compares the Butterworth lowpass with that in SciPy"""
+    scipy_signal = pytest.importorskip("scipy.signal")
+    b, a = scipy_signal.butter(order, 2 * math.pi * cutoff_frequency, analog=True, btype="lowpass")
+    s, h2 = scipy_signal.freqs(b, a)
+    f = s / (2 * math.pi)
+    c = sumpf.ButterworthFilter(cutoff_frequency=cutoff_frequency, order=order, highpass=False)
+    h1 = c(f)[0]
+    assert h1 == pytest.approx(h2)
+
+
+@hypothesis.given(cutoff_frequency=hypothesis.strategies.floats(min_value=1e-15, max_value=1e12),
                   order=hypothesis.strategies.integers(min_value=1, max_value=100))
 def test_highpass_properties(cutoff_frequency, order):
     """Checks the properties of a Butterworth highpass"""
@@ -64,6 +77,21 @@ def test_highpass_properties(cutoff_frequency, order):
     assert abs(f(cutoff_frequency)[0]) == pytest.approx(0.5 ** 0.5)
     # check the zero gain at zero frequency
     assert f(0.0)[0] == 0.0
+    # check the attenuation of low frequencies
+    assert abs(f(cutoff_frequency / 2)[0]) < abs(f(cutoff_frequency)[0]) < abs(f(cutoff_frequency * 2)[0])
     # check the spectrum generation
     spectrum = f.spectrum(resolution=cutoff_frequency / 5.0, length=10)
     assert (spectrum.channels()[0] == f(spectrum.frequency_samples())[0]).all()
+
+
+@hypothesis.given(cutoff_frequency=hypothesis.strategies.floats(min_value=1e-15, max_value=1e12),
+                  order=hypothesis.strategies.integers(min_value=1, max_value=16))
+def test_highpass_with_scipy(cutoff_frequency, order):
+    """Compares the Butterworth highpass with that in SciPy"""
+    scipy_signal = pytest.importorskip("scipy.signal")
+    b, a = scipy_signal.butter(order, 2 * math.pi * cutoff_frequency, analog=True, btype="highpass")
+    s, h2 = scipy_signal.freqs(b, a)
+    f = s / (2 * math.pi)
+    c = sumpf.ButterworthFilter(cutoff_frequency=cutoff_frequency, order=order, highpass=True)
+    h1 = c(f)[0]
+    assert h1 == pytest.approx(h2)
